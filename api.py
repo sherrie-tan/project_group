8cec0a2ffa6578bce57ff619e658179db200322a
sherrie-tan <sherrie2809@gmail.com>
2022-07-26 13:37:47 +0800

    added overhead.py

--- api.py
+++ api.py
@@ -1,4 +1,4 @@
 import requests
-url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=JPY&apikey=demo'
+url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=JPY&apikey=DD9WVI6K24NE4SZ7'
 r = requests.get(url)
 data = r.json()

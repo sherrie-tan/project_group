0b23b3067f5ac4250dc928bb448a37902f4004b2
kokojill <jillianyong2013@gmail.com>
2022-07-26 16:56:00 +0800

    fp exists

--- overheads.py
+++ overheads.py
@@ -1,5 +1,5 @@
 import csv
 from pathlib import Path
-fp = Path.cwd()/"money trees"/"csv_reports"/"overheads-day-40 to 50"
+fp = Path.cwd()/"pfb_group_proj"/"project_group"/"csv_reports"/"overheads-day-40 to 50"
 print(fp.exists())
 

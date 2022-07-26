0b028b3613dfacae07b0d27a8f32d1a1469d8056
sherrie-tan <sherrie2809@gmail.com>
2022-07-26 22:31:34 +0800

    nil

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,4 +1,12 @@
 import csv
 from pathlib import Path
 fp = Path.cwd()/"project_group"/"csv reports"/"Cash on Hand.csv"
+
 cash_on_hand_list = []
+
+with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+    reader = csv.reader(file)
+    for line in reader:
+        print(line)
+
+

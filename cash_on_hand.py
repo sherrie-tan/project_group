803b20906e0d9163ae33b41d973acb42e20bf454
sherrie-tan <sherrie2809@gmail.com>
2022-07-27 12:21:40 +0800

    changed fp

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,12 +1,5 @@
 import csv
 from pathlib import Path
-fp = Path.cwd()/"project_group"/"csv reports"/"Cash on Hand.csv"
+fp = Path.cwd()
 
-cash_on_hand_list = []
-
-with fp.open(mode="r", encoding="UTF-8", newline="") as file:
-    reader = csv.reader(file)
-    for line in reader:
-        print(line)
-
-
+print(fp)

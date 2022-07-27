843de5f8a84c0864819681086d34d7500b2f3049
sherrie-tan <sherrie2809@gmail.com>
2022-07-27 13:39:14 +0800

    added summaryreport.txt

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,5 +1,13 @@
 import csv
 from pathlib import Path
-fp = Path.cwd()/"project_group2"
-
+fp = Path.cwd()/"project_group"/"csv_reports"/"Cash on Hand.csv"
 print(fp)
+print(fp.exists())
+
+with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+    reader = csv.reader(file)
+
+    for line in reader:
+        print(line)
+
+

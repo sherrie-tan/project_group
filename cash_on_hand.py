3c765471c38ede32ab8b93f116c2d7385d8134f9
sherrie-tan <sherrie2809@gmail.com>
2022-07-27 14:12:55 +0800

    updated cashonhand.py

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -5,9 +5,19 @@ fp = Path.cwd()/"project_group"/"csv_reports"/"Cash on Hand.csv"
 
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
-
+    next(reader)
+    cash_on_hand_list =[]
     for line in reader:
-        print(line)
+       for value in line:
+        cash_on_hand_list.append(value)
+print(cash_on_hand_list)
 
-    cash_on_hand_list = [line]
-    print(cash_on_hand_list)
+
+
+
+
+    
+
+
+
+

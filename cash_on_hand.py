740fe312c33154b9568ccccfa1b47633791f3849
sherrie-tan <sherrie2809@gmail.com>
2022-07-28 13:38:00 +0800

    update coh

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,7 +1,7 @@
 import csv
 from pathlib import Path
 fp = Path.cwd()/"project_group"/"csv_reports"/"Cash on Hand.csv"
-
+print(fp)
 
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
@@ -15,7 +15,7 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     for sublist in cash_on_hand_list:
         print (sublist)
 
-    if 
+    
 
     
     

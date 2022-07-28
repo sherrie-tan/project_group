7f2550fab426b03e47165003043ec7d53318db2e
kokojill <jillianyong2013@gmail.com>
2022-07-28 08:43:18 +0800

    overheads.csv

--- overheads.py
+++ overheads.py
@@ -1,5 +1,6 @@
 import csv
 from pathlib import Path
-fp = Path.cwd()/"pfb_group_proj"/"project_group"/"csv_reports"/"overheads-day-40 to 50"
+fp = Path.cwd()/"pfb_group_proj"/"project_group"/"csv_reports"/"overheads.csv"
 print(fp.exists())
+
 

5700d7647e0b7c0ba4631f95ab86f5587404e9b5
sherrie-tan <sherrie2809@gmail.com>
2022-07-29 21:47:42 +0800

    update coh

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -1,19 +1,38 @@
 import csv
 from pathlib import Path
 fp = Path.cwd()/"project_group"/"csv_reports"/"Cash on Hand.csv"
-print(fp)
+#print(fp)
 
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader)
-    cash_on_hand_list = []
+    coh_list = []
     
     for line in reader:
-        cash_on_hand_list.append(line)
-    print (cash_on_hand_list)
+        coh_list.append(line)
+    #print(coh_list)
+    
+    coh_amt_list = []
+    for sublist in coh_list:
 
-    for sublist in cash_on_hand_list:
-        print (sublist)
+        coh_amt_list.append(int(sublist[1]))
+    print(coh_amt_list)
+
+    zipped_list = zip(coh_amt_list[:-1], coh_amt_list[1:])
+    diff_list = [next_element - first_element for first_element, next_element in zipped_list]
+    print(diff_list)
+
+    for sublist in diff_list:
+        if sublist < 0:
+            print(sublist)
+
+
+
+
+
+      
+
+    
 
     
 

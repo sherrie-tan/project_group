6f3923507b6b47b8851da3d622dedfee3db7dd23
sherrie-tan <sherrie2809@gmail.com>
2022-07-31 17:35:28 +0800

    left to remove neg sign in amt on report

--- cash_on_hand.py
+++ cash_on_hand.py
@@ -12,25 +12,26 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         coh_list.append(line)
     #print(coh_list)
     
+    day_list = []
     coh_amt_list = []
     for sublist in coh_list:
-
         coh_amt_list.append(int(sublist[1]))
-    print(coh_amt_list)
+        day_list.append(int(sublist[0]))
+    #print(day_list)
+    #print(coh_amt_list)
 
     zipped_list = zip(coh_amt_list[:-1], coh_amt_list[1:])
     diff_list = [next_element - first_element for first_element, next_element in zipped_list]
     print(diff_list)
-
-    for sublist in diff_list:
-        if sublist < 0:
-            print(sublist)
+    
 
 fp = Path.cwd()/"project_group"/"summary_report.txt"
  
 with fp.open(mode="w", encoding="UTF-8", newline="") as file:
     writer = csv.writer(file)
-    writer.writerow(["CASH DEFICT"])
+    for item in zip(day_list, diff_list):
+        if item[1] < 0:
+            writer.writerow(["[CASH DEFICT]" " "f"DAY: {item[0]+1}", " "f"AMOUNT: SGD{item[1]}"])
 
 
 

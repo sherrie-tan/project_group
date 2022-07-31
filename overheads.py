e8170e8b5d21fb86955d6ed2cba7d3d19dd29e18
kokojill <jillianyong2013@gmail.com>
2022-07-31 11:57:43 +0800

    added [] to highest overheads

--- overheads.py
+++ overheads.py
@@ -7,5 +7,5 @@ fp = Path.cwd()/"project_group"/"summary_report.txt"
  
 with fp.open(mode="a", encoding="UTF-8", newline="") as file:
     writer = csv.writer(file)
-    writer.writerow(["HIGHEST OVERHEADS"])
+    writer.writerow(["[HIGHEST OVERHEADS]"])
 

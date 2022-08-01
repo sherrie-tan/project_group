a8aec0efb30f39721c1d1b133e63ab0a83d6b9bb
sherrie-tan <sherrie2809@gmail.com>
2022-08-01 15:24:02 +0800

    solved conflicts in overheads.py

--- overheads.py
+++ overheads.py
@@ -3,16 +3,11 @@ from pathlib import Path
 fp = Path.cwd()/"project_group"/"csv_reports"/"Overheads.csv"
 
 
-<<<<<<< HEAD
-def overhead_function(forex):
-    fp = Path.cwd()/"project_group"/"csv_reports"/"Overheads.csv"
-
-=======
 # def overhead_function(forex):
 fp = Path.cwd()/"project_group"/"csv_reports"/"Overheads.csv"
 fp_write = Path.cwd()/"summary_report.txt"
 fp_write.touch()
->>>>>>> 39287ae9e848a524476af715d8308d5ade0c0580
+
 
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
@@ -42,13 +37,7 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
 fp = Path.cwd()/"project_group"/"summary_report.txt"
  
 with fp.open(mode="a", encoding="UTF-8", newline="") as file:
-<<<<<<< HEAD
-    file.write(f"\n[HIGHEST OVERHEADS] SALARY EXPENSE {highest_overheads}")
-    
 
-
-    
-=======
     file.write(f"\n[HIGHEST OVERHEADS] {category_list[maxValueIndex]}: SGD{maxValue}")
 
->>>>>>> 39287ae9e848a524476af715d8308d5ade0c0580
+

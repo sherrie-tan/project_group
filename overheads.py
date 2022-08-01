cbe110f806538f95d65a4c1c70db9c0a956e3549
kokojill <jillianyong2013@gmail.com>
2022-08-01 13:30:16 +0800

    found max overheads

--- overheads.py
+++ overheads.py
@@ -3,17 +3,23 @@ from pathlib import Path
 
 fp = Path.cwd()/"project_group"/"csv_reports"/"Overheads.csv"
 
-
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader)
-    overheads_list = []
+    main_overheads_list = []
 
     for line in reader:
-         overheads_list.append(line)
+         main_overheads_list.append(line)
+
+    overheads_list_USD = []
+    category_list = []
+    for sublist in main_overheads_list:
+        overheads_list_USD.append(float(sublist[1]))
+        category_list.append((sublist[0]))
     
 
-print(max(overheads_list))
+    print(max(overheads_list_USD))
+    print(max(category_list))
     
 
     

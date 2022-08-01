4623687d5ed610fde7239a8e0eaacad500b7123b
sherrie-tan <sherrie2809@gmail.com>
2022-08-01 13:14:25 +0800

    updated api

--- api.py
+++ api.py
@@ -6,23 +6,9 @@ url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_c
 response = requests.get(url)
 
 CURRENCY_EXCHANGE_RATE = response.json()
-    
-print(json.dumps(CURRENCY_EXCHANGE_RATE, indent=4))
 
-print(CURRENCY_EXCHANGE_RATE["Realtime Currency Exchange Rate"])
+print(CURRENCY_EXCHANGE_RATE)
 
-
-for eachIDD in CURRENCY_EXCHANGE_RATE["Realtime Currency Exchange Rate"]:
-    print (eachIDD)
-
-for eachIDD in CURRENCY_EXCHANGE_RATE["Exchange Rate"]:
-    print(eachIDD['Exchange Rate'])
-#for values in CURRENCY_EXCHANGE_RATE["Realtime Currency Exchange Rate"]:
-    #print(values['5. Exchange Rate'])
-
-
-
-#print(data["Realtime Currency Exchange Rate"])
 
 
 #fp = Path.cwd()/"project_group"/"summary_report.txt"

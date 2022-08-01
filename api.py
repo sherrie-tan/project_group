51f4adeaba2d7dcbba24c0eb6311e60a655732dc
sherrie-tan <sherrie2809@gmail.com>
2022-08-01 16:28:59 +0800

    updated api.py

--- api.py
+++ api.py
@@ -1,27 +1,27 @@
 from pathlib import Path
 import requests, json
 
-#def api_function():
-api_key = "DD9WVI6K24NE4SZ7"
-url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}'
+def api_function():
+    api_key = "DD9WVI6K24NE4SZ7"
+    url = f'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=SGD&apikey={api_key}'
 
-response = requests.get(url)
+    response = requests.get(url)
 
-CURRENCY_EXCHANGE_RATE = response.json()
+    CURRENCY_EXCHANGE_RATE = response.json()
 
-print(CURRENCY_EXCHANGE_RATE)
+    print(CURRENCY_EXCHANGE_RATE)
 
 
-for value in CURRENCY_EXCHANGE_RATE:
+    for value in CURRENCY_EXCHANGE_RATE:
         
-    CURRENCY_EXCHANGE_RATE = float(CURRENCY_EXCHANGE_RATE[value]["5. Exchange Rate"])
-    from_currency = CURRENCY_EXCHANGE_RATE[value]["1. From_Currency Code"]
-    to_currency = CURRENCY_EXCHANGE_RATE[value]["3. To_Currency Code"]
+        CURRENCY_EXCHANGE_RATE = float(CURRENCY_EXCHANGE_RATE[value]["5. Exchange Rate"])
+        #from_currency = CURRENCY_EXCHANGE_RATE[value]["1. From_Currency Code"]
+        #to_currency = CURRENCY_EXCHANGE_RATE[value]["3. To_Currency Code"]
 
 
 
-   # fp = Path.cwd()/"project_group"/"summary_report.txt"
+    fp = Path.cwd()/"project_group"/"summary_report.txt"
 
-   # with fp.open(mode="w",encoding="UTF-8",newline="") as file:
-        #file.write("[REAL TIME CURRENCY CONVERSION RATE]" " " f"{from_currency}1 = {to_currency}{CURRENCY_EXCHANGE_RATE}")
+    with fp.open(mode="w",encoding="UTF-8",newline="") as file:
+        file.write("[REAL TIME CURRENCY CONVERSION RATE]" " " f"USD1 = SGD{CURRENCY_EXCHANGE_RATE}")
 

c92f7205da68413da96209d73956a69c7091ac20
sherrie-tan <sherrie2809@gmail.com>
2022-08-02 21:45:39 +0800

    amend the fp

--- profit_loss.py
+++ profit_loss.py
@@ -1,33 +1,35 @@
 import csv
 from pathlib import Path
 
-fp_read = Path.cwd()/"project_group"/"csv_reports"/"Cash on Hand.csv"
+fp_read = Path.cwd()/"project_group"/"csv_reports"/"Profits and Loss.csv"
 fp_write = Path.cwd()/"project_group"/"summary_report.txt"
     
-def cash_on_hand_function(forex):
-    fp_read = Path.cwd()/"project_group"/"csv_reports"/"Cash on Hand.csv"
-    fp_write = Path.cwd()/"project_group"/"summary_report.txt"
+#def cash_on_hand_function(forex):
+fp_read = Path.cwd()/"project_group"/"csv_reports"/"Profits and Loss.csv"
+fp_write = Path.cwd()/"project_group"/"summary_report.txt"
     
-    with fp_read.open(mode="r", encoding="UTF-8", newline='') as file:
-        reader = csv.reader(file, skipinitialspace=True)
-        rows = list(reader)
-        next(rows)
+with fp_read.open(mode="r", encoding="UTF-8", newline='') as file:
+    reader = csv.reader(file)
 
-        profit_loss_list = []
+    next(reader)
 
-        for line in reader:
-            profit_loss_list.append(line)
+    profit_loss_list = []
 
-        day_list = []
-        net_profit_list = []
-        for sublist in profit_loss_list:
-            net_profit_list.append(int(sublist[4]))
-            day_list.append(int(sublist[0]))
+    for line in reader:
+        profit_loss_list.append(line)
+    #print(profit_loss_list)
+    
+    day_list = []
+    net_profit_list = []
+    
+    for sublist in profit_loss_list:
+        net_profit_list.append(int(sublist[4]))
+        day_list.append(int(sublist[0]))
 
-        diff_list = []
+    diff_list = []
 
-        for n in range(1, len(net_profit_list)):
-            diff_list.append(net_profit_list[n] - net_profit_list[n-1])
-        print(diff_list)
+    for n in range(1, len(net_profit_list)):
+        diff_list.append(net_profit_list[n] - net_profit_list[n-1])
+    print(diff_list)
 
         # not done

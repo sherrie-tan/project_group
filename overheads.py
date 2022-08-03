02a9428dd07c3ac4db92599be515ba27d1892aa6
kokojill <jillianyong2013@gmail.com>
2022-08-03 10:32:05 +0800

    solved overheads.py issues

--- overheads.py
+++ overheads.py
@@ -1,23 +1,20 @@
 import csv
 from pathlib import Path
-fp = Path.cwd()/"project_group"/"csv_reports"/"Overheads.csv"
-fp_write = Path.cwd()/"summary_report.txt"
-# fp_write.touch()
-
+fp_read = Path.cwd()/"project_group"/"csv_reports"/"Overheads.csv"
+fp_write = Path.cwd()/"project_group"/"summary_report.txt"
 
 def overhead_function(forex):
-  fp = Path.cwd()/"project_group"/"csv_reports"/"Overheads.csv"
-  fp_write = Path.cwd()/"summary_report.txt"
+    fp_read = Path.cwd()/"project_group"/"csv_reports"/"Overheads.csv"
+    fp_write = Path.cwd()/"project_group"/"summary_report.txt"
 
 
-with fp.open(mode="r", encoding="UTF-8", newline="") as file:
+    with fp_read.open(mode="r", encoding="UTF-8", newline="") as file:
         reader = csv.reader(file)
         next(reader)
         main_overheads_list = []
 
         for line in reader:
             main_overheads_list.append(line)
-    # print((main_overheads_list))
 
         overheads_list_USD = []
         category_list = []
@@ -25,17 +22,14 @@ with fp.open(mode="r", encoding="UTF-8", newline="") as file:
             overheads_list_USD.append(float(sublist[1]))
             category_list.append((sublist[0]))
     
-maxValue = max(overheads_list_USD)
-maxValueIndex = overheads_list_USD.index(maxValue)
-print(category_list[maxValueIndex])
-print(maxValue)
+    maxValue = max(overheads_list_USD)
+    maxValueIndex = overheads_list_USD.index(maxValue)
+# print(category_list[maxValueIndex])
+# print(maxValue)
 
     
-    # USD_to_SGD = maxValue*forex
-    
-
-fp = Path.cwd()/"project_group"/"summary_report.txt"
+    overheads_SGD = maxValue*forex
  
-#with fp.open(mode="a", encoding="UTF-8", newline="") as file:
-    #file.write(f"\n[HIGHEST OVERHEADS] {category_list[maxValueIndex]}: SGD{maxValue}")
+    with fp_write.open(mode="a", encoding="UTF-8", newline="") as file:
+      file.write(f"\n[HIGHEST OVERHEADS] {category_list[maxValueIndex]}: SGD{overheads_SGD}")
 

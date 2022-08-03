461ec90f3132ea79fa644f1aa43dee48ad7e5723
sherrie-tan <sherrie2809@gmail.com>
2022-08-03 11:08:30 +0800

    proof read

--- overheads.py
+++ overheads.py
@@ -16,16 +16,19 @@ def overhead_function(forex):
         for line in reader:
             main_overheads_list.append(line)
 
+    
         overheads_list_USD = []
         category_list = []
         for sublist in main_overheads_list:
             overheads_list_USD.append(float(sublist[1]))
             category_list.append((sublist[0]))
-    
+        print(overheads_list_USD)
+        print(category_list)
+
     maxValue = max(overheads_list_USD)
     maxValueIndex = overheads_list_USD.index(maxValue)
-# print(category_list[maxValueIndex])
-# print(maxValue)
+    print(category_list[maxValueIndex])
+    print(maxValue)
 
     
     overheads_SGD = maxValue*forex

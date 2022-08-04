6589d25684238a18de716eb396828c314ad8cf78
qiantongg <tqiantongg@gmail.com>
2022-08-04 20:26:39 +0800

    Commenting

--- profit_loss.py
+++ profit_loss.py
@@ -1,12 +1,12 @@
 import csv
 from pathlib import Path
 
-# create fp_read to read the csv file
+# creating fp_read to read the csv file
 fp_read = Path.cwd()/"project_group"/"csv_reports"/"Profits and Loss.csv"
-# create fp_write to write in summary report
+# creating fp_write to write in the summary report
 fp_write = Path.cwd()/"project_group"/"summary_report.txt"
 
-# create a function with forex as the parameter
+# creating a function with forex as the parameter
 def profitloss_function(forex):
     """
     This function will compute the difference in the net profit between each day
@@ -15,30 +15,31 @@ def profitloss_function(forex):
     fp_read = Path.cwd()/"project_group"/"csv_reports"/"Profits and Loss.csv"
     fp_write = Path.cwd()/"project_group"/"summary_report.txt"
     
-    # open the Profit and Loss.csv as file as read mode to read the data in it     
+    # opening the Profit and Loss.csv file in read mode to read the data in it     
     with fp_read.open(mode="r", encoding="UTF-8", newline='') as file:
         reader = csv.reader(file)
         
         # skip reading the headers
         next(reader)
         
-        # create an empty sublist to store data from profits and loss csv file
+        # creating an empty sublist to store data from profits and loss csv file
         profit_loss_list = []
 
-        # data is extracted and stored into profit_loss_list using append
+        # extracting data and storing into profit_loss_list using append.()
         for line in reader:
             profit_loss_list.append(line)
     
-        # create empty lists for day_list and net_profit_list to store the day and the net profit respectively 
+        # creating empty lists for day_list and net_profit_list to store the 
+        # number of days and the net profit respectively 
         day_list = []
         net_profit_list = []
         
-        # .append() the respective values into the empty list created before
+        # appending the respective values into the empty list created before using append.()
         for sublist in profit_loss_list:
             net_profit_list.append(int(sublist[4]))
             day_list.append(int(sublist[0]))
 
-        # create an empty sublist to store the difference in net profit
+        # creating an empty sublist to store the difference in net profit
         diff_list = []
 
         # range() and len() used to keep track of number of iterations to do

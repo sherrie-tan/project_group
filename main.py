4255f87adf63ae5747415439946e47fff441e026
sherrie-tan <sherrie2809@gmail.com>
2022-08-04 16:45:09 +0800

    documentation completed

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+# import modules 
 import api, cash_on_hand, overheads, profit_loss
 
-
+# define modules
 def main(): 
-    
+    """
+    This function will execute the codes in each module and write data 
+    into the summary report 
+    """
+    # assign api_function to forex with no parameters 
     forex = api.api_function()
     overheads.overhead_function(forex)
     cash_on_hand.cash_on_hand_function(forex)

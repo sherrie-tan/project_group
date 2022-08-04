6bd44c1524da035b402e63795769afdc9835a0e9
qiantongg <tqiantongg@gmail.com>
2022-08-04 23:15:41 +0800

    documentation

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     This function will execute the codes in each module and write data 
     into the summary report 
     """
-    # assign api_function to forex with no parameters 
+    # assigning api_function to forex with no parameters 
     forex = api.api_function()
     overheads.overhead_function(forex)
     cash_on_hand.cash_on_hand_function(forex)
